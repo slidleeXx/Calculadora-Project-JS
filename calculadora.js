b8aeb6b41081a1dc8a979481ddcb55f404e4bde4
SlidleeXx <AlejandroChurquina2503@gmail.com>
2025-09-24 22:14:20 -0300

    agregar mas numeros (ya funciona)

--- calculadora.js
+++ calculadora.js
@@ -1,67 +1,49 @@
-document.querySelector("#displey").innerText= "0"; 
-let valor1=0;
-let valor2=0;
-let operacion="";
+document.querySelector("#display").innerText= "0"; 
+let valor1 = 0;
+let valor2 = 0;
+let operacion = "";
 
- const btnNum1 = document.querySelector("#btnNum1");
- const btnSumar= document.querySelector("#btnSumar");
-const btnIgual= document.querySelector("#btnIgual");
-const btnNum2= document.querySelector("#btnNum2");
+const btnNum1 = document.querySelector("#btnNum1");
+const btnNum2 = document.querySelector("#btnNum2");
+const btnSumar = document.querySelector("#btnSumar");
+const btnIgual = document.querySelector("#btnIgual");
 
- function calcular (num1, num2, operacion) {
+function calcular(num1, num2, operacion) {
     switch (operacion) {
-        case "+":
-            return num1 + num2;
-        case "-":
-            return num1 - num2;
-
-        case "*":
-            return num1 * num2; 
-        case "/":
-            return num1 / num2; 
-        default:
-            return "Operacion no valida";
+        case "+": return num1 + num2;
+        case "-": return num1 - num2;
+        case "*": return num1 * num2; 
+        case "/": return num1 / num2; 
+        default:  return "Operacion no valida";
     }
 }
 
 function asignarValor(valor) {
-    if( !operacion || !valor) {
-        valor1=valor;
+    if (!operacion) {
+        valor1 = valor;
     } else {
-        valor2=valor;
+        valor2 = valor;
     }
 }
 
+btnNum1.addEventListener("click", (e) => {
+    document.querySelector("#display").innerText = e.target.innerText;
+    asignarValor(Number(e.target.innerText));
+});
 
- btnNum1.addEventListener("click", (e) => {
-    document.querySelector("#displey").innerText = e.target.innerText;
-    asignarValor( Number(e.target.innerText));
- });
+btnNum2.addEventListener("click", (e) => {  
+    document.querySelector("#display").innerText = e.target.innerText;
+    asignarValor(Number(e.target.innerText));
+});
 
- btnNum2.addEventListener("click", (e) => {  
-    document.querySelector("#displey").innerText = e.target.innerText;
-    asignarValor( Number(e.target.innerText));
-    });
-
- btnSumar.addEventListener("click", () => {  
+btnSumar.addEventListener("click", () => {  
     operacion = "+";
-    }); 
+}); 
 
 
-
-    btnIgual.addEventListener("click", () => {  
-
-        const resultado = calcular(valor1,valor2, operacion);
-        document.querySelector("#displey").innerText = resultado;
-        num1=resultado;
-        operacion="";
-
-        });
-
-
-
-
-
-
-
-
+btnIgual.addEventListener("click", () => {  
+    const resultado = calcular(valor1, valor2, operacion);
+    document.querySelector("#display").innerText = resultado;
+    valor1 = resultado; 
+    operacion = "";
+});
