c713477bbea945c8f977309b38f225d0e1fb813b
SlidleeXx <AlejandroChurquina2503@gmail.com>
2025-09-24 21:49:35 -0300

    Add initial implementation of a simple calculator with basic operations

---
+++ calculadora.js
@@ -0,0 +1,68 @@
+document.querySelector("#displey").innerText= "0"; 
+let num1=0;
+let num2=0;
+let operacion="";
+
+ const btnNum1 = document.querySelector("#btnNum1");
+ const btnSumar= document.querySelector("#btnSumar");
+const btnIgual= document.querySelector("#btnIgual");
+const btnNum2= document.querySelector("#btnNum2");
+
+ function calcular (num1, num2, operacion) {
+    switch (operacion) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+
+        case "*":
+            return num1 * num2; 
+        case "/":
+            return num1 / num2; 
+        default:
+            return "Operacion no valida";
+    }
+}
+
+function asignarValor(valor) {
+    if( !operacion || !valor1) {
+        valor1=valor;
+    } else {
+        valor2=valor;
+    }
+}
+
+
+ btnNum1.addEventListener("click", (e) => {
+    document.querySelector("#displey").innerText = e.target.innerText;
+    asignarValor( Number(e.target.innerText));
+ });
+
+ btnNum2.addEventListener("click", (e) => {  
+    document.querySelector("#displey").innerText = e.target.innerText;
+    asignarValor( Number(e.target.innerText));
+    });
+
+ btnSumar.addEventListener("click", (e) => {  
+    operacion = e.target.innerText;
+    document.querySelector("#displey").innerText = e.target.innerText;
+    });
+    
+
+
+    btnIgual.addEventListener("click", () => {  
+
+        const resultado = calcular(num1, num2, operacion);
+        document.querySelector("#displey").innerText = resultado;
+        num1=resultado;
+        operacion="";
+
+        });
+
+
+
+
+
+
+
+
